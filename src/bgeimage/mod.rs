2984e59aa9e4644ea5e7406041ff758ccb9b99dc
TNTSuperMan <abworld@outlook.jp>
2025-02-06 12:43:37 +0900

    inflateで圧縮展開をする

--- src/bgeimage/mod.rs
+++ src/bgeimage/mod.rs
@@ -1,5 +1,7 @@
 extern crate image;
 
+use inflate::inflate_bytes;
+
 mod tokenize;
 use tokenize::tokenize;
 
@@ -7,6 +9,7 @@ pub mod toimg;
 use toimg::tokens2imgs;
 
 pub fn bin2img(data: &[u8]) -> Vec<toimg::Bin>{
-    let token = tokenize(data);
+    let extracted = inflate_bytes(data).expect("Image extract error");
+    let token = tokenize(extracted.as_slice());
     return tokens2imgs(token);
 }
