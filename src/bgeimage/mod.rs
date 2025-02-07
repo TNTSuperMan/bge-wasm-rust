c143d54338f486aef949f4211fd72826ab7a0dd1
TNTSuperMan <abworld@outlook.jp>
2025-02-07 15:50:56 +0900

    inflateをzlibに修正

--- src/bgeimage/mod.rs
+++ src/bgeimage/mod.rs
@@ -1,6 +1,6 @@
 extern crate image;
 
-use inflate::inflate_bytes;
+use inflate::inflate_bytes_zlib;
 
 mod tokenize;
 use tokenize::tokenize;
@@ -12,7 +12,7 @@ pub fn bin2img(data: &[u8]) -> Vec<toimg::Bin>{
     let (lens, raw) = data.split_at(2);
     let image_len: u16 = ((lens[0] as u16) << 8) | lens[1] as u16;
     let (imgdata, _p) = raw.split_at(image_len as usize);
-    let extracted = inflate_bytes(imgdata).expect("Image extract error");
+    let extracted = inflate_bytes_zlib(imgdata).expect("Image extract error");
     let token = tokenize(extracted.as_slice());
     return tokens2imgs(token);
 }
