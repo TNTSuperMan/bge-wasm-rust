7c0ca3ed28712c2e92dcf58e785a98805529e84f
TNTSuperMan <abworld@outlook.jp>
2025-06-19 17:40:28 +0900

    houndでwav出力部分を入れとく

--- src/music/towav.rs
+++ src/music/towav.rs
@@ -1,9 +1,28 @@
-use wavegen::{wf, sine};
+use std::io::Cursor;
+use hound::{SampleFormat, WavSpec, WavWriter};
 use crate::bgeimage::toimg::Bin;
 use super::tokenize::Music;
 
 pub fn token2wav(token: Music) -> Result<Bin, String>{
-    sine
+    let mut samples: Vec<i16> = Vec::new();
+
+    // Samples generate code here
+
+    let mut buf = Cursor::new(Vec::new());
+    {
+        let mut writer = WavWriter::new(&mut buf, WavSpec {
+            channels: 1,
+            sample_rate: 44100,
+            bits_per_sample: 16,
+            sample_format: SampleFormat::Int,
+        }).unwrap();
+        for sample in samples {
+            writer.write_sample(sample).unwrap();
+        }
+        writer.finalize().unwrap();
+    }
+
+    return Ok(Bin::new(buf.into_inner()));
 }
 
 pub fn tokens2wavs(tokens: Vec<Music>) -> Result<Vec<Bin>, String>{
